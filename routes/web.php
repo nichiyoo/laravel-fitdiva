59a99868524454f5e52e470bf77d798a3414d4ec
nichiyoo <61191577+nichiyoo@users.noreply.github.com>
2025-04-15 12:21:23 +0700

    feat: add controllers, policies, requests, factories, and migrations for articles, courses, exercises, and users

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,14 @@
 
 use Illuminate\Support\Facades\Route;
 
+use Illuminate\Support\Facades\Auth;
+use App\Http\Controllers\Admin\UserController;
+use App\Http\Controllers\Admin\CourseController;
+use App\Http\Controllers\Admin\ExerciseController;
+use App\Http\Controllers\Admin\ArticleController;
+
 Route::get('/', fn() => redirect()->route('landing.index'))->name('welcome');
+Route::get('/dashboard', fn() => redirect()->route('admin.dashboard'))->name('home');
 
 Route::as('landing.')->group(function () {
     Route::get('/', fn() => view('landing.index'))->name('index');
@@ -13,4 +20,14 @@ Route::as('landing.')->group(function () {
 
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::middleware('auth')
+  ->prefix('admin')
+  ->as('admin.')
+  ->group(function () {
+    Route::get('/', fn() => view('home'))->name('dashboard');
+    Route::resource('users', UserController::class);
+    Route::resource('courses', CourseController::class);
+    Route::resource('exercises', ExerciseController::class);
+    Route::resource('articles', ArticleController::class);
+});
+
